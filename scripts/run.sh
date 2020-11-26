0490a15f50190e808bf997282c9f407b9a0b69df
Bluezdrive <bluezdrive@volkerfaas.de>
2020-11-26 08:22:35 +0100

    Update to kafka-cluster-manager-2.1

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-docker run --name kafka-cluster-manager -v "$(pwd)/topology:/home/topology" -v "$(pwd)/conf:/home/conf" bluezdrive/kafka-cluster-manager:2.0.1 "$@"
+docker run --name kafka-cluster-manager -v "$(pwd)/topology:/home/topology" -v "$(pwd)/conf:/home/conf" bluezdrive/kafka-cluster-manager:2.1 "$@"
 docker rm /kafka-cluster-manager > /dev/null 2>&1
